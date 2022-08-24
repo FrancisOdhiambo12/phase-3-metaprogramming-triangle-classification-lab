affb1d7a6b62a932b6342483fdb7fd628dc9116b
Francis <francisodhiambo2@gmail.com>
2022-08-24 18:50:05 +0300

    commit tree f1575e20696dd9ddd24c1f682fe7d312267c07af

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
   # write code here
+  attr_accessor
+  def initialize (first, second, third)
+    @first=first
+    @second=second
+    @third=third
+  end
+  def kind 
+    #error for negative sides
+    if @first <= 0 || @second <= 0 || @third <= 0
+      raise TriangleError
+    end
+    
+    if @first +@second <= @third || @first + @third <= @second || @second + @third <= @first
+      raise TriangleError
+    end
+    return :equilateral if @first == @second && @second == @third
+    return :isosceles if @first == @second || @second == @third || @first == @third
+    return :scalene if @first != @second && @second != @third && @second != @third
+    
+  end
+  class TriangleError < StandardError
+  end
 end
